87bb4a4f8fce828f8291d71bc8889cd65f153260
Chris <4070267+Chris-Holdt@users.noreply.github.com>
2023-06-17 12:36:35 +0100

    Custom CRKBD config, RP2040

---
+++ keyboards/crkbd/keymaps/choldt/keymap.c
@@ -0,0 +1,94 @@
+/*
+Copyright 2019 @foostan
+Copyright 2020 Drashna Jaelre <@drashna>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+
+// Define combos
+// const uint16_t PROGMEM ctrl_combo[] = {KC_A, KC_S, COMBO_END};
+const uint16_t PROGMEM alt_combo[] = {KC_A, KC_D, COMBO_END};
+// const uint16_t PROGMEM ctrlshft_combo[] = {KC_A, KC_W, COMBO_END};
+
+combo_t key_combos[] = {
+    // COMBO(ctrl_combo, KC_LCTL),
+    COMBO(alt_combo, KC_LALT),
+    // COMBO(ctrlshft_combo, LCTL(KC_LSFT)),
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT_split_3x6_3(
+  //,--------------------------------------------------------------------.                                  ,------------------------------------------------------------.
+      KC_GRV,                 KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                        KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
+  //|----------------------+--------+--------+--------+--------+--------|                                   |--------+--------+--------+--------+--------+---------------|
+      MT(MOD_LCTL, KC_ESC),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, LT(3, KC_NUBS),
+  //|----------------------+--------+--------+--------+--------+--------|                                   |--------+--------+--------+--------+--------+--------------|
+      KC_LSFT,                KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                        KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_QUOT,
+  //|----------------------+--------+--------+--------+--------+--------+--------------|           |--------+--------+--------+--------+--------+--------+-------------|
+                                                        TT(1), TT(2),  MT(MOD_LGUI, KC_SPC),      MT(MOD_LSFT, KC_BSPC), LT(1, KC_ENT), MT(MOD_LGUI, KC_TAB)
+                                                     //`-------------------------------'           `-------------------------------------------------'
+
+  ),
+
+    [1] = LAYOUT_split_3x6_3( // ^ = first char, $ = end of line, % = matching char e.g ({[]}), 0 = start of line
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+      KC_TRNS,  KC_1,   KC_2,     KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_TRNS,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_TRNS, KC_CIRC, KC_0,    KC_DLR, KC_PERC, KC_TRNS,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_TRNS,KC_TRNS,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          KC_TRNS, TO(0),  KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+    [2] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+      KC_TRNS, LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), CM_TOGG,         KC_TRNS, LGUI(KC_E), LGUI(KC_W), LGUI(KC_R), KC_TRNS, KC_TRNS,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_TRNS, KC_TRNS, KC_LPRN, KC_LCBR, KC_LBRC, KC_TRNS,                      KC_RBRC,  KC_RCBR, KC_RPRN, KC_NUHS, KC_TRNS,  KC_TRNS,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_GRV, KC_MINS, KC_UNDS,                      KC_EQL, KC_PLUS, KC_LT, KC_GT, KC_TRNS, KC_TRNS,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          KC_TRNS,   TO(0),  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+    [3] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+      QK_BOOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_TRNS, KC_TRNS, KC_SLEP, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_TRNS, KC_TRNS,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_MPLY, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+
+    [4] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+      QK_BOOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY, KC_TRNS, KC_MUTE,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS
+                                      //`--------------------------'  `--------------------------'
+  )
+};
--- keyboards/crkbd/rules.mk
+++ keyboards/crkbd/rules.mk
@@ -4,5 +4,10 @@
 
 DEFAULT_FOLDER = crkbd/rev1
 
-RGBLIGHT_SUPPORTED = yes
-RGB_MATRIX_SUPPORTED = yes
+CONVERT_TO = elite_pi
+
+# RGBLIGHT_SUPPORTED = yes
+# RGB_MATRIX_SUPPORTED = yes
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+# TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
